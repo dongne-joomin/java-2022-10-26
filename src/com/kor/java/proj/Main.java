01b1dc6843ece22fdcdecccfe59f56601145de1e
dong-joomin <donghunc88@gamil.com>
2022-10-26 21:28:09 +0900

    게시물 작성 기능 구현중

--- src/com/kor/java/proj/Main.java
+++ src/com/kor/java/proj/Main.java
@@ -1,4 +1,7 @@
 package com.kor.java.proj;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,18 +10,41 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		// Scanner : 입력값을 받음 
 		// System.in : 키보드의 입력값
-		System.out.printf("명령어를 입력 : ");
-		String command;
-		
-		command = sc.nextLine(); // 줄전체를 입력받아 출력
-		System.out.printf("입력된 명령어 : %s\n ", command);
-		command = sc.next(); // null값까지 받아 출력
-		System.out.printf("입력된 명령어 : %s\n ", command);
-		command = sc.next();
-		System.out.printf("입력된 명령어 : %s\n ", command);
-		
+		int lastArticleId = 0;
+		while( true ) {
+			System.out.printf("명령어를 입력 : ");
+			String command = sc.nextLine();
+			
+			command = command.trim();
+			
+			if ( command.length() == 0 ) {
+				continue;
+			}
+			
+			if ( command.equals("system exit") ) {
+				break;
+			}
+			else if ( command.equals("article write") ) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				
+				
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				id ++;
+			}
+			else if ( command.equals("article list") ) {
+				System.out.println("계시물이 없습니다.");
+			}
+			else  {
+				System.out.printf("%s(은)는 존재하지 않는 명령어 입니다.\n", command);
+		    }
+		}
 		sc.close();
 		System.out.println("== 프로그램 끝 ==");
 	}
 }
- 
